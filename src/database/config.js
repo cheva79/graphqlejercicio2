41a2efdd64880ee337e4aee3e168e7ca4dcf5489
David Echavarria <cheva79@gmail.com>
2021-12-01 19:15:36 -0500

    Cambio exitoso

--- src/database/config.js
+++ src/database/config.js
@@ -4,13 +4,8 @@ const dotenv = require('dotenv').config();
 
 const dbConnection = async () => {
     try {
-<<<<<<< HEAD
         await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pequx.mongodb.net/test`);
         console.log('DB Online'); 
-=======
-        await mongoose.connect('mongodb+srv://admin:*****@cluster0.pequx.mongodb.net/test');
-        console.log('DB Online');
->>>>>>> 9ebc78caa01236025a8e2ce8f00eea8bb970f3eb
     }
     catch (error) {
 
